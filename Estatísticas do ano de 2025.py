6a2fea687bc9e69967e679f8fadc7e32ea972899
Dannyboy-Tavares <tavaresboy@gmail.com>
2025-12-15 04:55:22 -0800

    Update Estatísticas do ano de 2025.py

--- Estatísticas do ano de 2025.py
+++ Estatísticas do ano de 2025.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d.axes3d import Axes3D
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
 
 st.title ("Estatísticas do ano de 2025")
