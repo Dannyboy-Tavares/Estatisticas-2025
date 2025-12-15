31e59f06d90e4b730a3462f3e8dfb909da37809b
Dannyboy-Tavares <tavaresboy@gmail.com>
2025-12-15 03:53:59 -0800

    Update Estatísticas do ano de 2025.py

--- Estatísticas do ano de 2025.py
+++ Estatísticas do ano de 2025.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.axes3d import Axes3D
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
 
-Menu = option_menu(
-        "Navegação", ["Iniciar", "Estatística", "Pesquisar", "Dados"],
-        menu_icon="cast",
-        icons=["house", "bar-chart", "search", "graph-up-arrow"],
-        default_index=0
-    )
 st.title ("Estatísticas do ano de 2025")
+
+Menu = option_menu(menu_title="Menu"
+        options=["Inicio", "Gráficos Estatística", "Gráficos dinâmicos", "Widgets", "Formulário"],
+        icons=["house", "bar-chart", "bar-chart-line", "toggles", "bar-chart"],
+        menu_icon='cast'
+        default_index=0
+        orientation="horizontal"
+    )
+
